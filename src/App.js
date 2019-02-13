7e31ac137279df345e51d58ca103ad1624081afb
Gustavo Leste <gustavoleste@protonmail.com>
2019-02-13 17:00:17 -0200

    layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import {
+        Button, 
+        H1, 
+        Row, 
+        Col,
+        Card,
+        CardBlock,
+        CardTitle,
+        CardText,
+        CardHeader
+} from '@bootstrap-styled/v4'
+import styled from 'styled-components'
 
 const app = () => (
-        <div>
-                <h1>Hello</h1>
-        </div>
+        <Container>                              
+                <Row >
+                        <Col>
+                                <Card>
+                                        <CardHeader>
+                                                <H1>React | Redux-Saga - Exemplo</H1>
+                                        </CardHeader>
+                                        <Row>
+                                                <Col >                                                                                                    
+                                                        <CardBlock>
+                                                                <CardTitle>Meu IP</CardTitle>
+                                                                <CardText>0.0.0.0</CardText>
+                                                                <Button>Submit</Button>
+                                                        </CardBlock>                                                
+                                                </Col> 
+                                                <Col >                                                                                                    
+                                                        <CardBlock>
+                                                                <CardTitle>Meu Navegador</CardTitle>
+                                                                <CardText>Desconhecido</CardText>
+                                                                <Button>Submit</Button>
+                                                        </CardBlock>                                                
+                                                </Col>
+                                        </Row>                                       
+                                </Card>
+                        </Col>
+                </Row>
+        </Container>
 )
 
 export default app
+
+const Container = styled.section`
+       text-align: center;
+       width: 80vw;
+       margin: auto;
+`
